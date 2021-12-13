328cbf5eaad2ab3de83806f86bc2563b8a07bf06
Bryan <andersonbr@gmail.com>
2021-12-12 22:18:23 -0600

    updated models.py

--- shipcomp/models.py
+++ shipcomp/models.py
@@ -9,21 +9,36 @@ class Vendor(models.Model):
     main_contact = models.CharField(50),
     phone_number = models.CharField(max_length=15)
 
+    def __str__(self):
+        return self.vendor_name
+
 class Item(models.Model):
     item_name = models.CharField(max_length=50),
     unit_price = models.IntegerField(5),
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE),
     item_description = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.item_name
+
 class Warehouse(models.Model):
+    warehouse_name = models.CharField(max_length=50),
     address = models.CharField(max_length=50),
     city = models.CharField(max_length=50),
     state = models.CharField(max_length=2)
 
+    def __str__(self):
+        return self.warehouse_name
+
+
 class DistributionCenter(models.Model):
+    dist_name = models.CharField(max_length=50),
     address = models.CharField(max_length=50),
     city = models.CharField(max_length=50),
     state = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.dist_name
 
 class Shipments(models.Model):
     dist_center = models.ForeignKey(DistributionCenter, on_delete=models.CASCADE),
@@ -33,10 +48,19 @@ class Shipments(models.Model):
     received = models.BooleanField(),
     shipping_weight = models.IntegerField()
 
+    def __str__(self):
+        return self.expected_arrival_date
+    def __bool__(self):
+        return self.received
+
+
 class ShippingDetails(models.Model):
     shipment = models.ForeignKey(Shipments, on_delete=models.CASCADE),
     item = models.ForeignKey(Item, on_delete=models.CASCADE),
     quantity = models.IntegerField(4)
+
+    def __int__(self):
+        return self.quantity
 
 
 
