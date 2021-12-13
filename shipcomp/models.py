5da6502e25d7c7ece62a59a333e5f28ae9b3681e
Bryan <andersonbr@gmail.com>
2021-12-12 22:07:47 -0600

    update 12/12/2021

--- shipcomp/models.py
+++ shipcomp/models.py
@@ -2,41 +2,41 @@ from django.db import models
 
 
 class Vendor(models.Model):
-    vendor_name = models.CharField(50),
-    city = models.CharField(50),
-    state = models.CharField(2),
-    country = models.CharField(25),
+    vendor_name = models.CharField(max_length=50),
+    city = models.CharField(max_length=50),
+    state = models.CharField(max_length=2),
+    country = models.CharField(max_length=25),
     main_contact = models.CharField(50),
-    phone_number = models.CharField(15)
+    phone_number = models.CharField(max_length=15)
 
 class Item(models.Model):
-    item_name = models.CharField(50),
-    unit_price = models.IntegerField(5),
-    vendor_id = models.ForeignKey(Vendor, on_delete=models.CASCADE),
-    item_description = models.CharField(100)
+    item_name = models.CharField(max_length=50),
+    unit_price = models.IntegerField(max_length=5),
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE),
+    item_description = models.CharField(max_length=100)
 
 class Warehouse(models.Model):
-    address = models.CharField(50),
-    city = models.CharField(50),
-    state = models.CharField(2)
+    address = models.CharField(max_length=50),
+    city = models.CharField(max_length=50),
+    state = models.CharField(max_length=2)
 
 class DistributionCenter(models.Model):
-    address = models.CharField(50),
-    city = models.CharField(50),
-    state = models.CharField(2)
+    address = models.CharField(max_length=50),
+    city = models.CharField(max_length=50),
+    state = models.CharField(max_length=2)
 
 class Shipments(models.Model):
-    dist_center_id = models.ForeignKey(DistributionCenter, on_delete=models.CASCADE),
+    dist_center = models.ForeignKey(DistributionCenter, on_delete=models.CASCADE),
     departure_date = models.DateTimeField,
     expected_arrival_date = models.DateTimeField,
-    warehouse_id = models.ForeignKey(Warehouse, on_delete=models.CASCADE),
+    warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE),
     received = models.BooleanField(),
     shipping_weight = models.IntegerField()
 
 class ShippingDetails(models.Model):
-    shipment_id = models.ForeignKey(Shipments, on_delete=models.CASCADE),
-    item_id = models.ForeignKey(Item, on_delete=models.CASCADE),
-    quantity = models.IntegerField(4)
+    shipment = models.ForeignKey(Shipments, on_delete=models.CASCADE),
+    item = models.ForeignKey(Item, on_delete=models.CASCADE),
+    quantity = models.IntegerField(max_length=4)
 
 
 
