7dd97e2ae0824652cdb23d009b4d750f078e5af5
Bryan <andersonbr@gmail.com>
2021-12-12 23:06:25 -0600

    updated admin.py

--- shipcomp/admin.py
+++ shipcomp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Item, Vendor, DistributionCenter, Shipments, ShippingDetails
+
+admin.site.register(Item)
+admin.site.register(Vendor)
+admin.site.register(DistributionCenter)
+admin.site.register(Shipments)
+admin.site.register(ShippingDetails)
+
 
 # Register your models here.
